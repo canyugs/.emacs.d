08a98e3e65f44e95e3bc075f9a4d780e9a3362d4
can.yu <fineaisa@gmail.com>
2019-10-03 11:55:38 +0800

    initial commit

---
+++ init.el
@@ -0,0 +1,28 @@
+;; Remove menu-bar
+(menu-bar-mode -1) 
+
+;; Use space instead of tabs
+(setq-default indent-tabs-mode nil)
+
+;; Magit, global key binding
+(global-set-key (kbd "C-x g") 'magit-status)
+
+
+;; Use Package Manager
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (magit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
